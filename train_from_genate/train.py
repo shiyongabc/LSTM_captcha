063de2c8c56a7991aaab261eb80ba037720398e0
shiyongabc <574773374@qq.com>
2018-12-08 14:57:20 +0800

    import

--- train_from_genate/train.py
+++ train_from_genate/train.py
@@ -213,7 +213,7 @@ def train_crack_captcha_cnn():
                 print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())), step, loss_)
                 print(u'***************************************************************第%s次的准确率为%s'%(step, acc))
                 # 如果准确率大于50%,保存模型,完成训练
-                if acc > 0.001:                  ##我这里设了0.9，设得越大训练要花的时间越长，如果设得过于接近1，很难达到。如果使用cpu，花的时间很长，cpu占用很高电脑发烫。
+                if acc > 0.9:                  ##我这里设了0.9，设得越大训练要花的时间越长，如果设得过于接近1，很难达到。如果使用cpu，花的时间很长，cpu占用很高电脑发烫。
                     saver.save(sess, model_path, global_step=step)
                     print(time.time()-start_time)
                     break
