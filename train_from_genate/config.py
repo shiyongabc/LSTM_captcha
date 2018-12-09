27e67db587d9a4051e3f6af0bf5ae451ac775846
shiyongabc <574773374@qq.com>
2018-12-09 21:32:30 +0800

    rafactor path

--- train_from_genate/config.py
+++ train_from_genate/config.py
@@ -7,7 +7,7 @@ path = os.getcwd()  #项目所在路径
 
 
 meta_graph_path = path + '/model/crack_capcha.model-37100.meta'   #测试结果存放路径 meta_graph
-model_path = path + '/model/checkpoint' #模型存放路径
+model_path = path + '/model/' #模型存放路径
 
 
 
