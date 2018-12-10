5ae6cdc61dd356fed55aa9dc8d034a3385ee788e
shiyongabc <574773374@qq.com>
2018-12-10 10:22:19 +0800

    rafactor path

--- train_from_genate/config.py
+++ train_from_genate/config.py
@@ -10,6 +10,12 @@ output_path=path + '/result'
 meta_graph_path = path + '/model/crack_capcha.model-37100.meta'   #测试结果存放路径 meta_graph
 model_path = path + '/model/' #模型存放路径
 
+#要识别的字符
+number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+ALPHABET = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
 batch_size = 64  #size of batch
 time_steps = 60   #unrolled through 28 time steps #每个time_step是图像的一行像素 height
 n_input = 160  #rows of 28 pixels  #width
@@ -17,7 +23,7 @@ image_channels = 1  # 图像的通道数
 captcha_num = 4 # 验证码中字符个数
 
 
-
+n_classes = len(number) + len(ALPHABET)    #类别分类
 
 
 
