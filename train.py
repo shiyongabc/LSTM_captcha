b8d4e9345506c7f06c03ebe6bf0cd2e7b7d5219e
shiyongabc <574773374@qq.com>
2018-12-10 11:07:34 +0800

    rafactor path

--- train.py
+++ train.py
@@ -22,25 +22,21 @@ def train():
 
     with tf.Session() as sess:  # 创建tensorflow session
         sess.run(init)
-        iter = 1
-        while iter < iteration:
+        step = 0
+        while true:
             batch_x, batch_y = get_batch()
             sess.run(opt, feed_dict={x: batch_x, y: batch_y})   #只运行优化迭代计算图
-            if iter %100==0:
-                los, acc, parg, yarg = sess.run([loss, accuracy, pre_arg, y_arg],feed_dict={x:batch_x,y:batch_y})
-                print("For iter ",iter)
-                print("Accuracy ",acc)
-                print("Loss ",los)
-                if iter % 1000 ==0:
-                    print("predict arg:",parg[0:10])
-                    print("yarg :",yarg[0:10])
-                print("__________________")
-                # if acc > 0.95:
-                #     print("training complete, accuracy:", acc)
-                #     break
-            if iter % 1000 == 0:   #保存模型
+            los, acc, parg, yarg = sess.run([loss, accuracy, pre_arg, y_arg],feed_dict={x:batch_x,y:batch_y})
+            if step % 100 ==0:
+                print("训练第%s,准确率为:%s", %(step, acc))
+
+            if acc > 0.9:
                 saver.save(sess, model_path, global_step=iter)
-            iter += 1
+                print("training complete, accuracy:", acc)
+                #     break
+        #    if iter % 1000 == 0:   #保存模型
+        #       saver.save(sess, model_path, global_step=iter)
+                step += 1
         # 计算验证集准确率
         valid_x, valid_y = get_batch(data_path=validation_path, is_training=False)
         print("Validation Accuracy:", sess.run(accuracy, feed_dict={x: valid_x, y: valid_y}))
