fcdb6d26ac1c28dbed6cf330c36f458d8482383a
shiyongabc <574773374@qq.com>
2018-12-10 10:12:44 +0800

    rafactor path

--- train_from_genate/config.py
+++ train_from_genate/config.py
@@ -11,6 +11,11 @@ meta_graph_path = path + '/model/crack_capcha.model-37100.meta'   #测试结果�
 model_path = path + '/model/' #模型存放路径
 
 batch_size = 64  #size of batch
+time_steps = 50   #unrolled through 28 time steps #每个time_step是图像的一行像素 height
+n_input = 100  #rows of 28 pixels  #width
+image_channels = 1  # 图像的通道数
+captcha_num = 4 # 验证码中字符个数
+n_classes = len(number) + len(ALPHABET)    #类别分类
 
 
 
