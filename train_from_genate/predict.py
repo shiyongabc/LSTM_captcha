974666e593fb08d832a26222d685919857e57377
shiyongabc <574773374@qq.com>
2018-12-10 14:01:00 +0800

    rafactor path

--- train_from_genate/predict.py
+++ train_from_genate/predict.py
@@ -82,5 +82,32 @@ def write_to_file(predict_list, file_list):
 
 
 
+def predict1():
+    output = crack_captcha_cnn()
+    saver = tf.train.Saver()
+    sess = tf.Session()
+    saver.restore(sess, tf.train.latest_checkpoint('.'))
+
+    while (1):
+
+        text, image = gen_captcha_text_and_image()
+        image.write(text, test_data_path+'/'+text + '.jpg')  # 写到文件
+        image = convert2gray(image)
+        image = image.flatten() / 255
+
+        predict = tf.argmax(tf.reshape(output, [-1, MAX_CAPTCHA, CHAR_SET_LEN]), 2)
+        text_list = sess.run(predict, feed_dict={X: [image], keep_prob: 1})
+        predict_text = text_list[0].tolist()
+        
+
+        vector = np.zeros(MAX_CAPTCHA * CHAR_SET_LEN)
+        i = 0
+        for t in predict_text:
+            vector[i * 63 + t] = 1
+            i += 1
+            # break
+
+        print("正确: {}  预测: {}".format(text, vec2text(vector)))
+
 if __name__ == '__main__':
-    predict()
+    predict1()
