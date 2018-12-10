5153c90c437a30aee0b88118cc22353a6b07acbe
shiyongabc <574773374@qq.com>
2018-12-10 09:36:05 +0800

    rafactor path

--- train_from_genate/config.py
+++ train_from_genate/config.py
@@ -6,6 +6,7 @@ import os
 path = os.getcwd()  #项目所在路径
 
 test_data_path=path + '/test_data'
+output_path=path + '/result'
 meta_graph_path = path + '/model/crack_capcha.model-37100.meta'   #测试结果存放路径 meta_graph
 model_path = path + '/model/' #模型存放路径
 
--- train_from_genate/predict.py
+++ train_from_genate/predict.py
@@ -26,17 +26,26 @@ def predict():
         image = image.flatten() / 255
 
         predict = tf.argmax(tf.reshape(output, [-1, MAX_CAPTCHA, CHAR_SET_LEN]), 2)
-        predict_text = text_list[0].tolist()
-        text_list = sess.run(predict, feed_dict={X: [image], keep_prob: 1})
-
+        #predict_text = text_list[0].tolist()
+        test_predict = sess.run(predict, feed_dict={X: [image], keep_prob: 1})
+        predict_result = []
         vector = np.zeros(MAX_CAPTCHA * CHAR_SET_LEN)
-        i = 0
-        for t in predict_text:
-            vector[i * 63 + t] = 1
-            i += 1
-            # break
+        for line in test_predict[0]:  # 将预测结果转换为字符
+            character = ""
+            for each in line:
+                character += index2char(each)
+            predict_result.append(character)
+        predict_result = predict_result[:len(file_list)]    #预测结果
+        write_to_file(predict_result, file_list)    #保存到文件
 
-        print("正确: {}  预测: {}".format(text, vec2text(vector)))
+def write_to_file(predict_list, file_list):
+    with open(output_path, 'a') as f:
+        for i, res in enumerate(predict_list):
+            if i == 0:
+                f.write("id\tfile\tresult\n")
+            f.write(str(i) + "\t" + file_list[i] + "\t" + res + "\n")
+    print("预测结果保存在：",output_path)
+
 
 if __name__ == '__main__':
     predict()
