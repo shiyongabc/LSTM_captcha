8e23449b4ccd90e7f4812400867dd066095fc7a5
shiyongabc <574773374@qq.com>
2018-12-12 13:25:20 +0800

    rafactor path

--- train_from_dianwang/train.py
+++ train_from_dianwang/train.py
@@ -146,21 +146,21 @@ def crack_captcha_cnn(w_alpha=0.01, b_alpha=0.1):
     # out_alpha = np.sqrt(2.0/1024)
 
     # 3 conv layer  N = (W − F + 2P )/S+1
-    w_c1 = tf.Variable(w_alpha * tf.random_normal([3, 3, 1, 32]))
+    w_c1 = tf.Variable(w_alpha * tf.random_normal([5, 5, 1, 32]))
     b_c1 = tf.Variable(b_alpha * tf.random_normal([32]))
     conv1 = tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(x, w_c1, strides=[1, 1, 1, 1], padding='SAME'), b_c1))
     conv1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
     conv1 = tf.nn.dropout(conv1, keep_prob)
     print(conv1.shape)
 
-    w_c2 = tf.Variable(w_alpha * tf.random_normal([3, 3, 32, 64]))
+    w_c2 = tf.Variable(w_alpha * tf.random_normal([5, 5, 32, 64]))
     b_c2 = tf.Variable(b_alpha * tf.random_normal([64]))
     conv2 = tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(conv1, w_c2, strides=[1, 1, 1, 1], padding='SAME'), b_c2))
     conv2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
     conv2 = tf.nn.dropout(conv2, keep_prob)
     print(conv2.shape)
 
-    w_c3 = tf.Variable(w_alpha * tf.random_normal([3, 3, 64, 64]))
+    w_c3 = tf.Variable(w_alpha * tf.random_normal([5, 5, 64, 64]))
     b_c3 = tf.Variable(b_alpha * tf.random_normal([64]))
     conv3 = tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(conv2, w_c3, strides=[1, 1, 1, 1], padding='SAME'), b_c3))
     conv3 = tf.nn.max_pool(conv3, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
@@ -168,7 +168,7 @@ def crack_captcha_cnn(w_alpha=0.01, b_alpha=0.1):
     print(conv3.shape)
 
     # Fully connected layer  8 * 32 * 40  10240  5120   372736
-    w_d = tf.Variable(w_alpha * tf.random_normal([372736, 1024]))
+    w_d = tf.Variable(w_alpha * tf.random_normal([7*7*64, 1024]))
     b_d = tf.Variable(b_alpha * tf.random_normal([1024]))
     dense = tf.reshape(conv3, [-1, w_d.get_shape().as_list()[0]])
     print(dense.shape)
