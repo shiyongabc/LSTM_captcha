dbcad48475ea8a1dec2ea5ee8287401939738c6c
shiyongabc <574773374@qq.com>
2018-12-15 09:46:50 +0800

    predit

--- train_from_dianwang/config.py
+++ train_from_dianwang/config.py
@@ -7,7 +7,7 @@ path = os.getcwd()  #项目所在路径
 
 test_data_path=path + '/test_data'
 output_path=path + '/result'
-meta_graph_path = path + '/model/crack_capcha.model-37100.meta'   #测试结果存放路径 meta_graph
+meta_graph_path = path + '/model/model.ckpt-126400.meta'   #测试结果存放路径 meta_graph
 model_path = path + '/model/model.ckpt' #模型存放路径
 moudke_file = path + '/model/' #模型存放路径
 
--- train_from_dianwang/predict.py
+++ train_from_dianwang/predict.py
@@ -134,4 +134,4 @@ def predict1():
 
 if __name__ == '__main__':
     #predict1()
-    predictFromPath('/root/python-shell/verifycode/dianwang/8.tif','8FhG')
+    predictFromPath('/root/python-shell/verifycode/dianwang/1.png','8FhG')
